98e29c5965584e8191ed0ddeb9c67c7fcf571e64
Heikki Salo <heikki.ao.salo@iki.fi>
2011-08-06 17:26:26 +0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vakkari2011::Application.config.secret_token = '9c2e8d1f3e287cc8efcdb99074e501d0b16e898ba58e6bf52185ec6ff7d9f1fc141585c0753e613bd6a415eee192076d7b8fcfa93f58d760c5d94ec04f1c4797'
