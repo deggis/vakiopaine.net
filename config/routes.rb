9c228f7f975dbd3ed78a7aeb9e2d147f661416fe
Heikki Salo <heikki.ao.salo@iki.fi>
2011-08-06 23:09:30 +0300

    Added home controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Vakkari2011::Application.routes.draw do
+  get "home/index"
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
