64e4dafc52955ddb948316a90c010d6fa70c527d
Heikki Salo <heikki.ao.salo@iki.fi>
2011-08-07 02:00:36 +0300

    Force Newsitems sorting by created_at @ admin view.

--- vendor/engines/newsitems/app/controllers/admin/newsitems_controller.rb
+++ vendor/engines/newsitems/app/controllers/admin/newsitems_controller.rb
@@ -2,7 +2,11 @@ module Admin
   class NewsitemsController < Admin::BaseController
 
     crudify :newsitem,
-            :title_attribute => 'title_fi', :xhr_paging => true
+            :title_attribute => 'title_fi',
+            :xhr_paging => true,
+            :order => "created_at DESC",
+            :sortable => false
+
 
   end
 end
