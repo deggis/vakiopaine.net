11876f3e52ab6478b6da50541374eeccc4ddade0
Heikki Salo <heikki.ao.salo@iki.fi>
2011-08-07 00:20:17 +0300

    Added Newsitem pagination.

--- vendor/engines/newsitems/app/controllers/newsitems_controller.rb
+++ vendor/engines/newsitems/app/controllers/newsitems_controller.rb
@@ -1,7 +1,9 @@
 class NewsitemsController < ApplicationController
 
-  before_filter :find_all_newsitems
+#  before_filter :find_all_newsitems
+  before_filter :find_first_newsitems
   before_filter :find_page
+
 
   def index
     # you can use meta fields from your model instead (e.g. browser_title)
@@ -17,7 +19,16 @@ class NewsitemsController < ApplicationController
     present(@page)
   end
 
+  def page
+    @newsitems = Newsitem.order('created_at DESC').offset(4*(params[:page].to_i)).limit(4)
+    present(@page)
+  end
+
 protected
+
+  def find_first_newsitems
+    @newsitems = Newsitem.order('created_at DESC').limit(4)
+  end
 
   def find_all_newsitems
     @newsitems = Newsitem.order('position ASC')
@@ -26,5 +37,6 @@ protected
   def find_page
     @page = Page.where(:link_url => "/newsitems").first
   end
+
 
 end
--- vendor/engines/newsitems/config/routes.rb
+++ vendor/engines/newsitems/config/routes.rb
@@ -1,6 +1,9 @@
 ::Refinery::Application.routes.draw do
   resources :newsitems, :only => [:index, :show]
 
+  match "/newsitems/sivu/:page" => "newsitems#page"
+  match "/newsitems/page/:page" => "newsitems#page"
+
   scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
     resources :newsitems, :except => :show do
       collection do
