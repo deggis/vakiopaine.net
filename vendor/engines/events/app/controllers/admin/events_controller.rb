8a48f763787ea12d9608a17c76b31bac513351fd
Heikki Salo <heikki.ao.salo@iki.fi>
2011-08-08 03:58:30 +0300

    Re-ordered events in admin page.

--- vendor/engines/events/app/controllers/admin/events_controller.rb
+++ vendor/engines/events/app/controllers/admin/events_controller.rb
@@ -2,7 +2,10 @@ module Admin
   class EventsController < Admin::BaseController
 
     crudify :event,
-            :title_attribute => 'title_fi', :xhr_paging => true
+            :title_attribute => 'title_fi',
+            :xhr_paging => true,
+            :order => "created_at DESC",
+            :sortable => false
 
   end
 end
