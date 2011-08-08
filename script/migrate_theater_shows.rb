07375fc6c50db01cff3c0ada0d1cf62d097b51d3
Heikki Salo <heikki.ao.salo@iki.fi>
2011-08-08 04:17:09 +0300

    Added script to migrate theater shows.

---
+++ script/migrate_theater_shows.rb
@@ -0,0 +1,18 @@
+$LOAD_PATH << "./"
+require 'mysql2'
+
+require 'migrate_helpers'
+require 'migrate_settings'
+
+client = Mysql2::Client.new(:host => db_host(), :username => db_user(), :password => db_pass(), :database => db_name())
+res = client.query("SELECT * from teatteri")
+
+results = res.as_json
+
+results.each do |row|
+    title_fi = row["nimi"]
+    desc_fi  = custom_htmlize(row["kuvaus"])
+    starts   = row["alkuaika"]
+    ends     = row["loppuaika"]
+    TheaterShow.create(:title_fi => title_fi, :description_fi => desc_fi, :starts => starts, :ends => ends)
+end
