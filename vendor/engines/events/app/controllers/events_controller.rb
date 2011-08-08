7043d8b0568c1ee3abcecb96bc4dbd6aa175d72b
Heikki Salo <heikki.ao.salo@iki.fi>
2011-08-08 04:00:52 +0300

    Events page show only upcoming events by default.

--- vendor/engines/events/app/controllers/events_controller.rb
+++ vendor/engines/events/app/controllers/events_controller.rb
@@ -2,6 +2,8 @@ class EventsController < ApplicationController
 
   before_filter :find_all_events
   before_filter :find_page
+  before_filter :find_upcoming_events
+  before_filter :find_past_events
 
   def index
     # you can use meta fields from your model instead (e.g. browser_title)
@@ -20,7 +22,15 @@ class EventsController < ApplicationController
 protected
 
   def find_all_events
-    @events = Event.order('position ASC')
+    @events = Event.order('date DESC')
+  end
+
+  def find_upcoming_events
+    @upcoming_events = Event.find(:all, :conditions => ["date > ?", 1.days.ago], :order => "date ASC")
+  end
+
+  def find_past_events
+    @past_events = Event.find(:all, :conditions => ["date < ?", Time.now], :order => "date ASC")
   end
 
   def find_page
