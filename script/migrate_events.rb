ad10bec35cdead6f9ee44f7fd7a470b7f42af77e
Heikki Salo <heikki.ao.salo@iki.fi>
2011-08-08 04:01:54 +0300

    Added script to migrate events.

---
+++ script/migrate_events.rb
@@ -0,0 +1,17 @@
+$LOAD_PATH << "./"
+require 'mysql2'
+
+require 'migrate_helpers'
+require 'migrate_settings'
+
+client = Mysql2::Client.new(:host => db_host(), :username => db_user(), :password => db_pass(), :database => db_name())
+res = client.query("SELECT * from tapahtumat")
+
+results = res.as_json
+
+results.each do |row|
+    title_fi   = row["tapahtuma"]
+    desc_fi    = custom_htmlize(row["kuvaus"])
+    date       = row["pvm"]
+    Event.create(:title_fi => title_fi, :desc_fi => desc_fi, :date => date)
+end
