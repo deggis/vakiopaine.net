bc1a0950b2c92fced27fc72dd0c71204b89d963d
Heikki Salo <heikki.ao.salo@iki.fi>
2012-05-23 18:43:04 +0300

    Enable duplicate titles in events.

--- vendor/engines/events/app/models/event.rb
+++ vendor/engines/events/app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ActiveRecord::Base
 
   acts_as_indexed :fields => [:title_fi, :desc_fi, :title_en, :desc_en]
 
-  validates :title_fi, :presence => true, :uniqueness => true
+  validates :title_fi, :presence => true
   
 end
