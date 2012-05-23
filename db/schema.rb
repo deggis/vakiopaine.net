16eff97be407f88675db4333b8df8a7b6a38850d
Heikki Salo <heikki.ao.salo@iki.fi>
2012-05-23 19:15:16 +0300

    Use Exhibition engine's page to display header snippets.

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -85,6 +86,7 @@ ActiveRecord::Schema.define(:version => 20110807161104) do
     t.datetime "updated_at"
   end
 
+  add_index "page_part_translations", ["locale"], :name => "index_page_part_translations_on_locale"
   add_index "page_part_translations", ["page_part_id"], :name => "index_page_part_translations_on_page_part_id"
 
   create_table "page_parts", :force => true do |t|
@@ -108,6 +110,7 @@ ActiveRecord::Schema.define(:version => 20110807161104) do
     t.datetime "updated_at"
   end
 
+  add_index "page_translations", ["locale"], :name => "index_page_translations_on_locale"
   add_index "page_translations", ["page_id"], :name => "index_page_translations_on_page_id"
 
   create_table "pages", :force => true do |t|
